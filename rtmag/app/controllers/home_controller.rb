dd364e49690c2b756949956100906525e1ea7298
BerylSystems <vinay@berylsystems.com>
2015-04-21 18:42:59 +0530

    Integration of Instagram and Location field and Changes in Images Shown -Now Images shown is also fetched from Instagram. -Now there is also addition of Location field in Create your own. -And changes in Images shown and now there is a main image at top and then in middle images fetched is shown in all field.

--- rtmag/app/controllers/home_controller.rb
+++ rtmag/app/controllers/home_controller.rb
@@ -6,8 +6,25 @@ class HomeController < ApplicationController
 
   def result
     get_client
-      @tweet = @client.search("#{params[:key]} filter:images", :include_entities=>true, :result_type => "recent").take(50)
-   end
+    unless params[:address].blank?
+      lat = Geocoder.search(params[:address]).first.coordinates.first
+      lng = Geocoder.search(params[:address]).first.coordinates.last
+      @tweet = @client.search("#{params[:key]} filter:images", :include_entities=>true, :geocode => "#{lat},#{lng},10mi", :result_type => "recent").take(20)
+    else
+      @tweet = @client.search("#{params[:key]} filter:images", :include_entities=>true, :result_type => "recent").take(20)
+    end
+
+    client = Instagram.client(:access_token => session[:access_token])
+
+    unless params[:key].blank?
+      tags = client.tag_search(params[:key])
+      @tag = client.tag_recent_media(tags[0].name, count: 50)
+    else  
+      lat = Geocoder.search(params[:address]).first.coordinates.first
+      lng = Geocoder.search(params[:address]).first.coordinates.last
+      @tag=client.media_search(lat, lng, options = {:count => 5})
+    end  
+  end
 
   def get_client
     @client = Twitter::REST::Client.new do |config|
